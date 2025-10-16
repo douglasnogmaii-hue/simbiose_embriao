29025549278ecfd8cced7bb7778fa650a3670b5e
douglasnogmaii-hue <douglas.nogmaii@gmail.com>
2025-10-16 01:06:30 -0300

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -1,10 +1,6 @@
 // bridge.js
-// Função de envio simulada para a API
-
-function enviar_para_API(mensagem) {
-    // Simulação de envio
-    console.log(`Mensagem enviada para API: ${mensagem}`);
+function enviarParaAPI(mensagem) {
+  console.log(`Mensagem enviada para API: ${mensagem}`);
 }
 
-// Exporta a função para uso externo
-module.exports = { enviar_para_API };
+module.exports = { enviarParaAPI };
